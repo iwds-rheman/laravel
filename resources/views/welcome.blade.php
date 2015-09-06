057f1cc6418e2877868b08e63e23307da0c14767
bcgoodmate <genesis.revelation89@gmail.com>
2015-09-07 05:59:50 +0800

    Splash and Dashboard Index pages changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<title>IPM | Internal Project Management v1.0 beta</title>
+		<title>IPM | Internal Project Management &beta; v1.0</title>
 
 		<link href="dashboard/css/bootstrap.min.css" rel="stylesheet">
 		<link href="dashboard/font-awesome/css/font-awesome.css" rel="stylesheet">
@@ -20,7 +20,7 @@
 					<h1 class="logo-name">IPM</h1>
 				</div>
 				
-				<h3>Internal Project Management <sup>v1.0 beta</sup></h3>
+				<h3>Internal Project Management <sup>&beta; v1.0</sup></h3>
 				
 				<form class="m-t" role="form" action="http://localhost/laravel/dashboard/">
 					<div class="form-group">
